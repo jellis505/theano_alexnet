4e86bbc53a3602e459415678b4f8b2068b376fe3
Joe Ellis <joe.ellis.505@gmail.com>
2015-04-02 12:52:19 -0400

    Added some additional layers, etc. to the library

--- alex_net.py
+++ alex_net.py
@@ -6,7 +6,161 @@ import theano.tensor as T
 
 import numpy as np
 
-from layers import DataLayer, ConvPoolLayer, DropoutLayer, FCLayer, SoftmaxLayer
+from layers import DataLayer, ConvPoolLayer, DropoutLayer, FCLayer, SoftmaxLayer,
+                   LogisticRegressionLayer, Deconvolutional_Layer, GaussianLayer
+
+class Variational_AlexNet(object):
+    """ This implements a variational version of alexnet """
+    def __init__(self, config):
+
+        self.config = config
+
+        batch_size = config['batch_size']
+        flag_datalayer = config['use_data_layer']
+        lib_conv = config['lib_conv']
+
+        ###################### BUILD NETWORK ##########################
+        
+        ###################### Variational Network ####################
+        # allocate symbolic variables for the data
+        # 'rand' is a random array used for random cropping/mirroring of data
+        x = T.ftensor4('x')
+        y = T.lvector('y')
+        eps = T.fmatrix('eps')
+        rand = T.fvector('rand')
+
+        print '... building the model'
+        self.layers = []
+        params = []
+        weight_types = []
+
+        if flag_datalayer:
+            data_layer = DataLayer(input=x, image_shape=(3, 256, 256,
+                                                         batch_size),
+                                   cropsize=227, rand=rand, mirror=True,
+                                   flag_rand=config['rand_crop'])
+
+            layer1_input = data_layer.output
+        else:
+            layer1_input = x
+
+        convpool_layer1 = ConvPoolLayer(input=layer1_input,
+                                        image_shape=(3, 227, 227, batch_size), 
+                                        filter_shape=(3, 11, 11, 96), 
+                                        convstride=4, padsize=0, group=1, 
+                                        poolsize=3, poolstride=2, 
+                                        bias_init=0.0, lrn=True,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer1)
+        params += convpool_layer1.params
+        weight_types += convpool_layer1.weight_type
+
+        convpool_layer2 = ConvPoolLayer(input=convpool_layer1.output,
+                                        image_shape=(96, 27, 27, batch_size),
+                                        filter_shape=(96, 5, 5, 256), 
+                                        convstride=1, padsize=2, group=1, 
+                                        poolsize=3, poolstride=2, 
+                                        bias_init=0.1, lrn=True,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer2)
+        params += convpool_layer2.params
+        weight_types += convpool_layer2.weight_type
+
+        convpool_layer3 = ConvPoolLayer(input=convpool_layer2.output,
+                                        image_shape=(256, 13, 13, batch_size),
+                                        filter_shape=(256, 3, 3, 384), 
+                                        convstride=1, padsize=1, group=1, 
+                                        poolsize=1, poolstride=0, 
+                                        bias_init=0.0, lrn=False,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer3)
+        params += convpool_layer3.params
+        weight_types += convpool_layer3.weight_type
+
+        convpool_layer4 = ConvPoolLayer(input=convpool_layer3.output,
+                                        image_shape=(384, 13, 13, batch_size),
+                                        filter_shape=(384, 3, 3, 384), 
+                                        convstride=1, padsize=1, group=1, 
+                                        poolsize=1, poolstride=0, 
+                                        bias_init=0.1, lrn=False,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer4)
+        params += convpool_layer4.params
+        weight_types += convpool_layer4.weight_type
+
+        convpool_layer5 = ConvPoolLayer(input=convpool_layer4.output,
+                                        image_shape=(384, 13, 13, batch_size),
+                                        filter_shape=(384, 3, 3, 256), 
+                                        convstride=1, padsize=1, group=1, 
+                                        poolsize=3, poolstride=2, 
+                                        bias_init=0.0, lrn=False,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer5)
+        params += convpool_layer5.params
+        weight_types += convpool_layer5.weight_type
+
+        fc_layer6_input = T.flatten(
+            convpool_layer5.output.dimshuffle(3, 0, 1, 2), 2)
+        fc_layer6 = FCLayer(input=fc_layer6_input, n_in=9216, n_out=4096)
+        self.layers.append(fc_layer6)
+        params += fc_layer6.params
+        weight_types += fc_layer6.weight_type
+
+        # Dropout Layer no parameters just random drop out
+        dropout_layer6 = DropoutLayer(fc_layer6.output, n_in=4096, n_out=4096)
+
+        # Now we need to get the output or what we want z to be
+        # Now here is the Guassian Probability Section with the gaussian layer
+        gauss_layer = GaussianLayer(input=dropout_layer6.output, n_in=4096, n_out=4096)
+        self.layers.append(gauss_layer)
+        params += gauss_layer.params
+        weight_types += fc_layer6.weight_type
+
+        ############ Sampling Layer ##########################
+        #Create the sampled output layer
+        z = gauss_layer.mu_encoded + T.exp(gauss_layer.log_sig_encoded) * eps
+        prior_z = 0.5* T.sum(1 + 2*gauss_layer.log_sig_encoded - gauss_layer.mu_encoded**2 - T.exp(2*gauss_layer.log_sig_encoded)) 
+
+        ############ GENERATIVE MODEL ########################
+        fc_layer_d1 = FClayer(input=z, n_in=4096, n_out=9216)
+        self.layers.append(fc_layer_d1)
+        params += fc_layer_d1.params
+        weight_types += fc_layer_d1.weight_type
+
+        # De_Convolutional Layer 1
+        deconvpool_layer1 = Deconvolutional_Layer(input=fc_layer_d1.output,
+                                        channel=1,
+                                        filter_shape=(384, 3, 3, 256), 
+                                        convstride=1, padsize=1, group=2, 
+                                        poolsize=3, poolstride=2, 
+                                        bias_init=0.0, lrn=False,
+                                        lib_conv=lib_conv,
+                                        )
+        self.layers.append(convpool_layer5)
+        params += convpool_layer5.params
+        weight_types += convpool_layer5.weight_type
+
+
+
+
+
+        ### Sample Layers
+
+        ##################### NETWORK BUILT #######################
+
+        self.cost = prior_z
+        self.errors = z
+        self.params = params
+        self.x = x
+        self.y = y
+        self.rand = rand
+        self.weight_types = weight_types
+        self.batch_size = batch_size
 
 
 class AlexNet(object):
--- lib/layers.py
+++ lib/layers.py
@@ -9,6 +9,10 @@ from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
 from pylearn2.sandbox.cuda_convnet.pool import MaxPool
 from pylearn2.expr.normalize import CrossChannelNormalization
 
+# This is is the portion for deconvolutional Layer. Using Raw Theano and not pylearn2...
+from theano.tensor.signal.downsample import max_pool_2d
+from theano.tensor.nnet import conv2d
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -38,7 +42,6 @@ class Weight(object):
         print 'weight loaded: ' + name
         self.np_values = np.load(dir + name + '.npy')
         self.val.set_value(self.np_values)
-
 
 class DataLayer(object):
 
@@ -297,3 +300,200 @@ class SoftmaxLayer(object):
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
+
+class LogisticRegressionLayer(object):
+    """ This layer is implemented if we do not want to apply any type of 
+    restriction to only one class being available at one time """
+    
+    def __init__(self, invals, n_in, n_out):
+        self.W = Weight((n_in, n_out))
+        self.b = Weight((n_out,), std=0)
+
+        self.p_y_given_x = T.nnet.sigmoid(T.dot(invals, self.W.val) + self.b.val))
+        
+        self.params = [self.W.val, self.b.val]
+        self.weight_type = ['W', 'b']
+        print 'logistic regression layer with num_in: ' + str(n_in) + \
+            'num classes: ' + str(n_out)
+
+    def negative_log_likelihood(self, y):
+        return -T.sum( T.nnet.binary_crossentropy(self.p_y_given_x, y))
+
+class GaussianLayer(object):
+    """ This layer is the output of a guassian layer, which predicts the 
+    mu and sigma for the auto-encoder given that the input from a hidden layer"""
+    def __init__(self, invals, n_in, n_out):
+        
+        # Initialize the output here
+        self.W_mu = Weight((n_in, n_out))
+        self.b_mu = Weight((n_out, ), std=0)
+        self.W_sig = Weight((n_in, n_out))
+        self.b_sig = Weight((n_out,), std=0)
+
+        # This is the mean and standard deviation of the representation
+        self.mu_encoded = T.dot(invals, self.W_mu.val) + self.b_mu.val
+        self.sig_encoded = T.dot(invals, self.W_sig.val) + self.b_sig.val
+        self.log_sig_encoded = 0.5*self.sig_encoded
+
+        self.params = [self.W_mu, self.b_mu, self.W_sig, self.b_sig]
+        self.weight_type = ['W', 'b', 'W', 'b']
+        print "Gaussian Layer with num_in: " + str(n_in) + \
+              " num_z_dim: " + str(n_out)
+
+
+class Deconvolutional_Layer(object):
+    """ This layer implements a deconvolution for working with and taking the 
+    derivatives of the function data here """
+
+    def __init__(self, input, channel, filter_shape, convstride, padsize,
+                 group, poolsize, poolstride, bias_init, lrn=False,
+                 lib_conv='cudnn',
+                 ):
+        '''
+        lib_conv can be cudnn (recommended)or cudaconvnet
+        '''
+
+        self.filter_size = filter_shape
+        self.convstride = convstride
+        self.padsize = padsize
+        self.poolsize = poolsize
+        self.poolstride = poolstride
+        self.channel = channel
+        self.lrn = lrn
+        self.lib_conv = lib_conv
+        assert group in [1, 2]
+
+        self.filter_shape = np.asarray(filter_shape)
+        #self.image_shape = np.asarray(image_shape)
+
+        if self.lrn:
+            self.lrn_func = CrossChannelNormalization()
+
+        if group == 1:
+            self.W = Weight(self.filter_shape)
+            self.b = Weight(self.filter_shape[3], bias_init, std=0)
+        else:
+            self.filter_shape[0] = self.filter_shape[0] / 2
+            self.filter_shape[3] = self.filter_shape[3] / 2
+            self.image_shape[0] = self.image_shape[0] / 2
+            self.image_shape[3] = self.image_shape[3] / 2
+            self.W0 = Weight(self.filter_shape)
+            self.W1 = Weight(self.filter_shape)
+            self.b0 = Weight(self.filter_shape[3], bias_init, std=0)
+            self.b1 = Weight(self.filter_shape[3], bias_init, std=0)
+
+        if lib_conv == 'cudaconvnet':
+            print "This isn't implemented with cudaconvnet"
+
+        elif lib_conv == 'cudnn':
+
+            input_shuffled = input.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+            # in01out to outin01
+            # print image_shape_shuffled
+            # print filter_shape_shuffled
+            if group == 1:
+                W_shuffled = self.W.val.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+                conv_out = self.deconv_and_depool(input_shuffled, W_shuffled, self.b.val.dimshuffle('x', 0, 'x', 'x'))
+                # conv_out = dnn.dnn_conv(img=input_shuffled,
+                #                         kerns=W_shuffled,
+                #                         subsample=(convstride, convstride),
+                #                         border_mode=padsize,
+                #                         )
+                # conv_out = conv_out + self.b.val.dimshuffle('x', 0, 'x', 'x')
+            else:
+                W0_shuffled = \
+                    self.W0.val.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+                conv_out0 = \
+                    dnn.dnn_conv(img=input_shuffled[:, :self.channel / 2,
+                                                    :, :],
+                                 kerns=W0_shuffled,
+                                 subsample=(convstride, convstride),
+                                 border_mode=padsize,
+                                 )
+                conv_out0 = conv_out0 + \
+                    self.b0.val.dimshuffle('x', 0, 'x', 'x')
+                W1_shuffled = \
+                    self.W1.val.dimshuffle(3, 0, 1, 2)  # c01b to bc01
+                conv_out1 = \
+                    dnn.dnn_conv(img=input_shuffled[:, self.channel / 2:,
+                                                    :, :],
+                                 kerns=W1_shuffled,
+                                 subsample=(convstride, convstride),
+                                 border_mode=padsize,
+                                 )
+                conv_out1 = conv_out1 + \
+                    self.b1.val.dimshuffle('x', 0, 'x', 'x')
+                conv_out = T.concatenate([conv_out0, conv_out1], axis=1)
+
+            # ReLu
+            self.output = conv_out #T.maximum(conv_out, 0)
+
+            # Pooling -- This is unnecessary it's already built into my model
+            #if self.poolsize != 1:
+            #    self.output = dnn.dnn_pool(self.output,
+            #                               ws=(poolsize, poolsize),
+            #                               stride=(poolstride, poolstride))
+
+            self.output = self.output.dimshuffle(1, 2, 3, 0)  # bc01 to c01b
+
+        else:
+            NotImplementedError("lib_conv can only be cudaconvnet or cudnn")
+
+        # LRN
+        if self.lrn:
+            # lrn_input = gpu_contiguous(self.output)
+            self.output = self.lrn_func(self.output)
+
+        if group == 1:
+            self.params = [self.W.val, self.b.val]
+            self.weight_type = ['W', 'b']
+        else:
+            self.params = [self.W0.val, self.b0.val, self.W1.val, self.b1.val]
+            self.weight_type = ['W', 'b', 'W', 'b']
+
+        print "deconv ({}) layer with shape_in:".format(lib_conv)
+
+    def deconv(self, X, w, b=None):
+        s = int(np.floor(w.val.get_value().shape[-1]/2.))
+        z = dnn_conv(X, w, direction_hint="*not* 'forward!",
+                     border_mode=s)
+        #s = int(np.floor(w.get_value().shape[-1]/2.))
+        #z = conv2d(X, w, border_mode='full')[:, :, s:-s, s:-s]
+        #z = conv2d(X, w, border_mode='full')
+        if b is not None:
+            z += b.dimshuffle('x', 0, 'x', 'x')
+        return z
+
+    def depool(self, X, factor=2):
+        """
+        luke perforated upsample
+        http://www.brml.org/uploads/tx_sibibtex/281.pdf
+        """
+        output_shape = [
+            X.shape[1],
+            X.shape[2]*factor,
+            X.shape[3]*factor
+        ]
+        stride = X.shape[2]
+        offset = X.shape[3]
+        in_dim = stride * offset
+        out_dim = in_dim * factor * factor
+
+        upsamp_matrix = T.zeros((in_dim, out_dim))
+        rows = T.arange(in_dim)
+        cols = rows*factor + (rows/stride * factor * offset)
+        upsamp_matrix = T.set_subtensor(upsamp_matrix[rows, cols], 1.)
+
+        flat = T.reshape(X, (X.shape[0], output_shape[0], X.shape[2] * X.shape[3]))
+
+        up_flat = T.dot(flat, upsamp_matrix)
+        upsamp = T.reshape(up_flat, (X.shape[0], output_shape[0],
+                                     output_shape[1], output_shape[2]))
+
+        return upsamp
+
+    def deconv_and_depool(self, X, w, b=None, factor=2):
+        return T.clip(self.deconv(self.depool(X, factor), w, b), -1., 1.)
+
+
+
